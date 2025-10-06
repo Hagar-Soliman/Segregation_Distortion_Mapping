f9fcc4406791fb5fbfc8873c05074ac2ae1b3b94
Hagar Soliman <164038550+Hagar-Soliman@users.noreply.github.com>
2025-10-06 14:04:33 -0400

    Create bam_list_loop.sh

---
+++ Loops/bam_list_loop.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+BASE_DIR="/home/hks25/palmer_scratch/libs/aligned"
+PREFIX="2A"
+OUTPUT_DIR="/home/hks25/palmer_scratch/SNP_calling"
+OUTPUT_FILE="${OUTPUT_DIR}/${PREFIX}_bam_list.txt"
+
+# Clear or create the output file
+> "$OUTPUT_FILE"
+
+# Loop through each ${PREFIX}*_aligned directory
+for dir in "$BASE_DIR"/${PREFIX}*_aligned; do
+    if [[ -d "$dir" ]]; then
+        echo "Scanning: $dir"
+        find "$dir" -type f -name "*.bam" >> "$OUTPUT_FILE"
+    fi
+done
+
+echo "BAM list created at: $OUTPUT_FILE"
